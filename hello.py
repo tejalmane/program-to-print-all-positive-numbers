11e5abf20bfcc5b66b59aeab94d0ee06361898f7
unknown <tejal.mane77@gmail.com>
2023-05-22 00:32:41 +0530

    output display changes

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 list1=[12,-7,5,64,-14]
 list2=[12,14,-95,3]
+print("original list"+ str(list1))
 num1=list(filter(lambda x:x>0,list1))
+print("original list"+ str(list2))
 num2=list(filter(lambda x:x>0,list2))
-print( num1)
-print(num2)
+print("output of list1" + str(num1))
+print("output of list2" + str(num2))
