736d029bd800b0736c054e1ac82cb3c78af95787
unknown <tejal.mane77@gmail.com>
2023-05-22 01:05:17 +0530

    removed changes

--- hello.py
+++ hello.py
@@ -2,7 +2,6 @@ list1=[12,-7,5,64,-14]
 list2=[12,14,-95,3]
 print("original list that we have to do changes"+ str(list1))
 num1=list(filter(lambda x:x>0,list1))
-print("original list"+ str(list2))
 num2=list(filter(lambda x:x>0,list2))
-print("output of list1" + str(num1))
-print("output of list2" + str(num2))
+print( num1)
+print(num2)
